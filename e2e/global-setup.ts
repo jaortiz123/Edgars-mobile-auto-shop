a914360ea50b198d550330b2ebfb3320569006b2
Jesus <www.jesus_012@hotmail.com>
2025-06-23 21:04:53 -0700

    feat: add reset-and-seed route and integration tests

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -3,6 +3,5 @@ import wait from './wait-for-backend'
 
 export default async function globalSetup() {
   await wait()
-  await axios.post('http://localhost:5001/debug/reset-db')
-  await axios.post('http://localhost:5001/debug/seed')
+  await axios.post('http://localhost:5001/debug/reset-and-seed')
 }
