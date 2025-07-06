1f2cd02f1e48844f280586170eae5c9fdf4dda0f
jaortiz123 <www.jesus_012@hotmail.com>
2025-07-06 11:24:31 -0700

    Remove old backend and prepare for serverless backend

--- backend/src/logger.js
+++
@@ -1,5 +0,0 @@
-const { createLogger, transports, format } = require('winston');
-module.exports = createLogger({
-  format: format.combine(format.timestamp(), format.json()),
-  transports: [new transports.Console()]
-});
